6fe57618f1fab799bdcfc6cea181f57fc46cfb59
Enrico <tappiera00@gmail.com>
2017-07-01 20:53:46 +0200

    added variables for backtesting

--- backtest/bot_backtest.py
+++ backtest/bot_backtest.py
@@ -2,17 +2,23 @@ import os.path, sys, getopt
 
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir))+'/utilities/')
 from bot_chart import BotChart
+from bot_arguments import BotArguments
 
 sys.path.append(os.path.abspath(os.path.join(os.getcwd(), os.pardir))+'/strategy/')
 from bot_strategy import BotStrategy
 
 def main(argv):
-    chart = BotChart("poloniex", "BTC_XMR", 300)
+	args = BotArguments(argv)
+	
+	chart = BotChart(args.exchange, args.pair, 
+					args.period, args.starttime, 
+					args.endtime)
+	
+	strategy = BotStrategy()
+	for candlestick in chart.getPoints():
+		strategy.tick(candlestick)
 
-    strategy = BotStrategy()
-
-    for candlestick in chart.getPoints():
-        strategy.tick(candlestick)
+	strategy.showProfit()
 
 if __name__ == "__main__":
 	main(sys.argv[1:])
